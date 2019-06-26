04941a23ab4676f15668ca297a938f66769930a6
One-Lemon <qq982516861@gmail.com>
2019-06-26 21:16:07 +0800

    0

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import reducer from './reducer';
-
+//combineReducers 管理 reducer
+// applyMiddleware 使用中间件
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export default createStore(
